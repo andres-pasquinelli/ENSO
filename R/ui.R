0989adef1edcf3116c26958f3e581192616f5fae
wedelia <pasquinelli@a-w-a.com.ar>
2018-06-03 13:33:44 -0300

    Input Select

--- R/ui.R
+++ R/ui.R
@@ -3,7 +3,7 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
@@ -11,23 +11,29 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+
   # Application title
   titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
+
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
        sliderInput("bins",
                    "Number of bins:",
                    min = 1,
                    max = 50,
-                   value = 30)
+                   value = 30),
+       selectInput("department", "Choose a department:",
+                   list(`Santa Fe` = c("Rosario", "N/A"),
+                        `Cordoba` = c("N/A", "N/A"),
+                        `Buenos Aires` = c("N/A","N/A"))
+       )
     ),
-    
+
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("distPlot"),
+       textOutput("result")
     )
   )
 ))
